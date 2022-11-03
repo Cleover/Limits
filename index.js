eb7ca8490f451032119b1b49a36c06331a2bbdb6
Cleo <cleo.debeau@gmail.com>
2022-11-02 21:08:55 -0300

    Main

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { Client } = require('discord.js');
 
-const client = new Client({});
+const client = new Client({
+    intents: [],
+});
 
 require('dotenv').config();
 
@@ -20,25 +22,25 @@ client.on('ready', () => {
 
     setInterval(async () => {
 
-    let messageDeleteQueue = [];
+        let messageDeleteQueue = [];
 
-    let lastMessageId = null;
-    while (true) {
-        let messages = await guildChannel.messages.fetch({
-            limit: 100,
-            before: lastMessageId
-        });
-        if (messages.size === 0) {
-            break;
+        let lastMessageId = null;
+        while (true) {
+            let messages = await guildChannel.messages.fetch({
+                limit: 100,
+                before: lastMessageId
+            });
+            if (messages.size === 0) {
+                break;
+            }
+            lastMessageId = messages.last().id;
+            messages.forEach(message => {
+                if (message.createdTimestamp < Date.now() - 1000 * 60 * 60 * 24 * 7) messageDeleteQueue.push(message.id);
+            })
         }
-        lastMessageId = messages.last().id;
-        messages.forEach(message => {
-            if (message.createdTimestamp < Date.now() - 1000 * 60 * 60 * 24 * 7) messageDeleteQueue.push(message.id);
-        })
-    }
 
 
-    await guildChannel.bulkDelete(messageDeleteQueue, true);
+        await guildChannel.bulkDelete(messageDeleteQueue, true);
 
     }, 1000 * 60 * 60);
 });
